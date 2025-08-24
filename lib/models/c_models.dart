4c609d5d4fbf54d9feadf1f3a19c3b281c2f397f
M7M00DZ <m7modhany283@gmail.com>
2025-08-24 19:25:04 +0300

    make sport model & services

---
+++ lib/models/c_models.dart
@@ -0,0 +1,8 @@
+class c_models {
+  
+  String? title;
+  String? subtitle;
+  String? url;
+
+  c_models({this.title, this.subtitle, this.url});
+}
--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_application_1/models/c_models.dart';
 import 'package:flutter_application_1/models/news_model.dart';
 
 class NewsServices {
@@ -19,6 +20,25 @@ class NewsServices {
         image: article['urlToImage'],
       );
       generalNews.add(GeneralList);
+    }
+    return generalNews;
+  }
+
+  Future<List<c_models>> getSportNews() async {
+    Response response = await dio.get(
+      'https://newsapi.org/v2/top-headlines/sources?category=sports&apiKey=9e33e104a56b434ca8b0030e2ec7acd2',
+    );
+    Map<String, dynamic> datafromJSON = response.data;
+    List<dynamic> articles = datafromJSON['sources'];
+
+    List<c_models> generalNews = [];
+    for (var article in articles) {
+      c_models sportList = c_models(
+        title: article['title'],
+        subtitle: article['description'],
+        url: article['urlToImage'],
+      );
+      generalNews.add(sportList);
     }
     return generalNews;
   }
